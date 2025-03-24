28b0cf10e09a3305366eea1c4fd27b7364178bcc
Big-Plato <luisoctavius.sc@gmail.com>
2025-03-24 19:14:33 -0300

    fix: class ship

--- src/modules/ship.js
+++ src/modules/ship.js
@@ -1,5 +1,28 @@
 export default class Ship {
-    constructor() {
+    constructor(length) {
+        this.length = length;
+        this.numberOfHits = 0;
+        this.sunk = false;
+    }
 
+    getLength() {
+        return this.length;
+    }
+
+    numberOfHits() {
+        return this.numberOfHits;
+    }
+
+    hit() {
+        this.numberOfHits++;
+    }
+
+    isSunk() {
+        if (this.numberOfHits == this.length) {
+            this.sunk = true;
+            return this.sunk;
+        } else {
+            return false;
+        }
     }
 }
