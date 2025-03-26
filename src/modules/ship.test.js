91da9444ac4dc1740a73db9d0ef5d71036e08da9
Big-Plato <luisoctavius.sc@gmail.com>
2025-03-26 20:50:15 -0300

    test: fix error in isSunk test

--- src/modules/ship.test.js
+++ src/modules/ship.test.js
@@ -1,11 +1,10 @@
 import Ship from "./ship.js";
 
-const ship1 = new Ship(4);
+const ship1 = new Ship("Battleship", 0, 0);
 
-test('expect isSunk to equal true with ship been hit 4 times', () => {
-    for (let i = 0; i < 4; i++) {
-        ship1.hit();
-    }
-    expect(ship1.isSunk()).toBeTruthy();
+test("expect isSunk to equal true with ship been hit 4 times", () => {
+  for (let i = 0; i < 4; i++) {
+    ship1.hit();
+  }
+  expect(ship1.isSunk()).toBeTruthy();
 });
-
