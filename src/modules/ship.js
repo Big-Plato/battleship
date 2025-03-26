2daca03290dccaeb3e5dd827626f7b6b41890c96
Big-Plato <luisoctavius.sc@gmail.com>
2025-03-26 20:49:02 -0300

    feat: create other classes to specific ships  that extend ship class

--- src/modules/ship.js
+++ src/modules/ship.js
@@ -1,28 +1,72 @@
 export default class Ship {
-    constructor(length) {
-        this.length = length;
-        this.numberOfHits = 0;
-        this.sunk = false;
-    }
+  constructor(name, x, y) {
+    this.name = name;
+    this.numberOfHits = 0;
+    this.sunk = false;
+    this.type = this.shipType(name);
+    this.x = x;
+    this.y = y;
+  }
 
-    getLength() {
-        return this.length;
-    }
+  numberOfHits() {
+    return this.numberOfHits;
+  }
 
-    numberOfHits() {
-        return this.numberOfHits;
+  shipType(name) {
+    switch (name) {
+      case "Carrier":
+        return new Carrier();
+      case "Battleship":
+        return new Battleship();
+      case "Destroyer":
+        return new Destroyer();
+      case "Submarine":
+        return new Submarine();
     }
+  }
 
-    hit() {
-        this.numberOfHits++;
-    }
+  hit() {
+    this.numberOfHits++;
+  }
 
-    isSunk() {
-        if (this.numberOfHits == this.length) {
-            this.sunk = true;
-            return this.sunk;
-        } else {
-            return false;
-        }
+  isSunk() {
+    if (this.numberOfHits == this.type.length) {
+      this.sunk = true;
+      return this.sunk;
+    } else {
+      return false;
     }
+  }
+
+  shipObj() {
+    return {};
+  }
+}
+
+class Carrier extends Ship {
+  constructor(len = 5) {
+    super();
+    this.len = len;
+  }
+}
+
+class Battleship extends Ship {
+  constructor(len = 4) {
+    super();
+    this.len = len;
+  }
+}
+
+class Destroyer extends Ship {
+  constructor(len = 3) {
+    super();
+    this.len = len;
+  }
+}
+
+class Submarine extends Ship {
+  constructor(len = 2) {
+    super();
+    this.len = len;
+  }
 }
