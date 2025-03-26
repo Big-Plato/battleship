7126e8668463a9b9bdc0dcd76f0d0f3e0c6e8162
Big-Plato <luisoctavius.sc@gmail.com>
2025-03-26 20:48:18 -0300

    feat: putShip and setShip methods

--- src/modules/gameBoard.js
+++ src/modules/gameBoard.js
@@ -1,36 +1,50 @@
-import Ship from "./ship.js"
+import Ship from "./ship.js";
 
 export class Gameboard {
-    constructor() {
-        this.game = new Board();
-        this.board = this.game.board();
-    }
+  constructor() {
+    this.game = new Board();
+    this.board = this.game.board();
+    this.ships = [];
+  }
 
-    receiveAttack(x, y) {
-        const board = this.board;
-        if (x > 9 || x < 0 || y > 9 || y < 0) {
-            throw new Error("Invalid coordenate!");
-        }
-        board[x][y] = "X";
-    }
+  setShip(name, x, y) {
+    checkError(x, y);
+    const ship = new Ship(name, x, y);
+    this.ships.push(ship);
+  }
 
-    getBoard() {
-        return this.board;
-    }
+  receiveAttack(x, y) {
+    checkError(x, y);
+    this.board[x][y] === 0 ? "X" : "A";
+  }
+
+  getBoard() {
+    return this.board;
+  }
 }
 
 class Board {
-    constructor() {
-    }
+  constructor() {}
 
-    board() {
-        const table = [];
-        for (let i = 0; i < 10; i++) {
-            table[i] = [];
-            for (let j = 0; j < 10; j++) {
-                table[i].push(0);
-            }
-        }
-        return table;
+  board() {
+    const table = [];
+    for (let i = 0; i < 10; i++) {
+      table[i] = [];
+      for (let j = 0; j < 10; j++) {
+        table[i].push(0);
+      }
     }
+    return table;
+  }
 }
+
+const checkError = (x, y) => {
+  if (x > 9 || x < 0 || y > 9 || y < 0) {
+    throw new Error("Invalid move!");
+  }
+};
+
+const randomizer = () => {
+  const random = Math.floor(Math.random() * 2);
+  return random;
+};
