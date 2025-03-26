d50a485ed22491b1a937c448dc00b9eed574accd
Big-Plato <luisoctavius.sc@gmail.com>
2025-03-26 20:50:44 -0300

    test: add tests for gameboard class

---
+++ src/modules/gameBoard.test.js
@@ -0,0 +1,30 @@
+import { Gameboard } from "./gameBoard.js";
+import { Ship } from "./ship.js"
+
+describe("Gameboard", () => {
+  const gameboard = new Gameboard();
+  const board = gameboard.board;
+
+  const expected = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ];
+
+  test("The board is generated with zeros", () => {
+    expect(board).toEqual(expected);
+  });
+
+  test('Horizontal ship', () => {
+    expect(gameboard.setShip("Destroyer", 0, 0))
+  })
+
+  test('')
+});
