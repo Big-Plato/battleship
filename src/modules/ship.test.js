c729bae180f4eea4929538f8bc34d4cdef528254
Big-Plato <luisoctavius.sc@gmail.com>
2025-03-26 21:12:05 -0300

    fix: change iterate of four to ship.length

--- src/modules/ship.test.js
+++ src/modules/ship.test.js
@@ -2,9 +2,11 @@ import Ship from "./ship.js";
 
 const ship1 = new Ship("Battleship", 0, 0);
 
-test("expect isSunk to equal true with ship been hit 4 times", () => {
-  for (let i = 0; i < 4; i++) {
+test("expect isSunk to equal true with battleship been hit 4 times", () => {
+  for (let i = 0; i < ship1.length; i++) {
     ship1.hit();
   }
   expect(ship1.isSunk()).toBeTruthy();
 });
+
+// test("expect isSunk to equal true with destroyer ")
