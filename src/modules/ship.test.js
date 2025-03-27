6f7a8d8ac1e7675d305c3d2e5701f7fafd2dec59
Big-Plato <luisoctavius.sc@gmail.com>
2025-03-27 17:55:19 -0300

    test: add test for destroyer ship

--- src/modules/ship.test.js
+++ src/modules/ship.test.js
@@ -1,6 +1,7 @@
 import Ship from "./ship.js";
 
 const ship1 = new Ship("Battleship", 0, 0);
+const ship2 = new Ship("Destroyer", 0, 0);
 
 test("expect isSunk to equal true with battleship been hit 4 times", () => {
   for (let i = 0; i < ship1.length; i++) {
@@ -9,4 +10,9 @@ test("expect isSunk to equal true with battleship been hit 4 times", () => {
   expect(ship1.isSunk()).toBeTruthy();
 });
 
-// test("expect isSunk to equal true with destroyer ")
+test("expect isSunk to equal true with destroyer been hit 3 times", () => {
+  for (let i = 0; i < ship2.length; i++) {
+    ship2.hit();
+  }
+  expect(ship2.isSunk()).toBeTruthy();
+});
