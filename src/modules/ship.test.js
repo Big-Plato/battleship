f81aae632573576be90db4fde27ecd986e7b8c89
Big-Plato <luisoctavius.sc@gmail.com>
2025-03-28 20:33:23 -0300

    style: prettier

--- src/modules/ship.test.js
+++ src/modules/ship.test.js
@@ -1,18 +1,19 @@
 import Ship from "./ship.js";
 
-const ship1 = new Ship("Battleship", 0, 0);
-const ship2 = new Ship("Destroyer", 0, 0);
+describe("isSunk return true if ship has received hits equal to his length", () => {
+  const ship1 = new Ship("Battleship", 0, 0);
+  const ship2 = new Ship("Destroyer", 0, 0);
+  test("expect isSunk to equal true with battleship been hit 4 times", () => {
+    for (let i = 0; i < ship1.length; i++) {
+      ship1.hit();
+    }
+    expect(ship1.isSunk()).toBeTruthy();
+  });
 
-test("expect isSunk to equal true with battleship been hit 4 times", () => {
-  for (let i = 0; i < ship1.length; i++) {
-    ship1.hit();
-  }
-  expect(ship1.isSunk()).toBeTruthy();
-});
-
-test("expect isSunk to equal true with destroyer been hit 3 times", () => {
-  for (let i = 0; i < ship2.length; i++) {
-    ship2.hit();
-  }
-  expect(ship2.isSunk()).toBeTruthy();
+  test("expect isSunk to equal true with destroyer been hit 3 times", () => {
+    for (let i = 0; i < ship2.length; i++) {
+      ship2.hit();
+    }
+    expect(ship2.isSunk()).toBeTruthy();
+  });
 });
