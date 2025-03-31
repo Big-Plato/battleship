05f766e150b6666d9861795ab1ed9d4e54e25721
Big-Plato <luisoctavius.sc@gmail.com>
2025-03-31 18:07:12 -0300

    feat: method to get the board

--- src/modules/player.js
+++ src/modules/player.js
@@ -1,23 +1,27 @@
-import { Gameboard } from "./gameBoard.js"
+import { Gameboard } from "./gameBoard.js";
 
 class Player {
-    constructor(name) {
-        this.name = name;
-        this.board = new Gameboard();
-    }
+  constructor(name) {
+    this.name = name;
+    this.board = new Gameboard();
+  }
+
+  getBoard() {
+    return this.board;
+  }
 }
 
 class Real extends Player {
-    constructor() {
-        super();
-    }
+  constructor() {
+    super();
+  }
 }
 
 class Computer extends Player {
-    constructor(name = "Computer") {
-        super();
-        this.name = name;
-    }
+  constructor(name = "Computer") {
+    super();
+    this.name = name;
+  }
 }
 
-export { Player, Real, Computer }
+export { Player, Real, Computer };
