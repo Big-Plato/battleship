fa9bf4618e8de65f24f5aa542db8fa6486cc5f14
Big-Plato <luisoctavius.sc@gmail.com>
2025-03-31 22:43:48 -0300

    feat: button to randomize the ships

--- src/modules/game.js
+++ src/modules/game.js
@@ -1,54 +1,61 @@
-import { generateBoard } from "./domManipulation.js";
 import { Real, Computer } from "./player.js";
+import { start, typeWriter, hideBoard, generateBoard, generateRandomShips } from "./domManipulation.js";
+
+const divBoard = document.querySelector(".board-div");
+const pBoard = document.querySelector(".board");
+const ctrlDiv = document.querySelector(".ctrl-btn");
+const info = document.querySelector("#info");
+
+const p1Board = document.querySelector("#p1-board");
+const p2Board = document.querySelector("#p2-board");
 
 export const game = () => {
-  const divBoard = document.querySelector(".board-div");
-  const pBoard = document.querySelector(".board");
-
-  const p1Board = document.querySelector("#p1-board");
-  const p2Board = document.querySelector("#p2-board");
-
+  start.innerHTML = "";
+  
   const player1 = new Real();
   const player2 = new Computer();
 
   const player1Board = player1.board;
   const player2Board = player2.board;
-  console.table(player1Board);
-  console.table(player2Board);
 
   generateRandomShips(player1Board);
   generateRandomShips(player2Board);
 
   generateBoard(p1Board, player1Board);
   generateBoard(p2Board, player2Board);
+
+  typeWriter(start, 'Begin');
+
+  hideBoard();
+  playButtons();
+
+  const randomBtn = document.querySelector("#random-btn");
+  typeWriter(randomBtn, 'Randomize')
+
+  randomBtn.addEventListener("click", () => {
+    player1.board.eraseBoard();
+    p1Board.innerHTML = "";
+    generateRandomShips(player1Board);
+    generateBoard(p1Board, player1Board);
+  });
+
+  start.removeEventListener("click", game);
+  start.addEventListener("click", () => {
+    randomBtn.remove();
+    start.remove();
+  });
+
+  switchTurns();
 };
 
-const randomizer = (num) => {
-  const random = Math.floor(Math.random() * num);
-  return random;
-};
+const playButtons = () => {
+    const randomizeBtn = document.createElement("button");
+    randomizeBtn.setAttribute("id", "random-btn");
+    ctrlDiv.insertBefore(randomizeBtn, start);
+}
 
-const generateRandomShips = (playerBoard) => {
-  const ships = ["Destroyer", "Battleship", "Submarine", "Carrier"];
+const switchTurns = (Player = 'Ressens') => {
+    typeWriter(info, `${Player}'s turn`)
+}
 
-  for (let i = 0; i < ships.length; i++) {
-    const shipName = ships[i];
-    let placed = false;
-    let attempts = 0;
-    const maxAttempts = 100;
 
-    const direction = i % 2 === 0 ? "Horizontal" : "Vertical";
-
-    while (!placed && attempts < maxAttempts) {
-        try {
-            playerBoard.setShip(shipName, randomizer(10), randomizer(10), direction);
-            placed = true;
-        } catch (err) {
-            attempts++;
-            if (attempts >= maxAttempts) {
-                console.error(`Failed to place ${shipName} after ${maxAttempts} attempts.`)
-            }
-        }
-    }
-  }
-};
