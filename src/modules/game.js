4caecb17a588a4e62de75f7ae88fa524ca5b341c
Big-Plato <luisoctavius.sc@gmail.com>
2025-04-01 22:39:46 -0300

    feat: the game is playable, but with some bugs that need fix like the flow of the game

--- src/modules/game.js
+++ src/modules/game.js
@@ -7,8 +7,6 @@ import {
   triggerGlitch,
 } from "./domManipulation.js";
 
-const divBoard = document.querySelector(".board-div");
-const pBoard = document.querySelector(".board");
 const ctrlDiv = document.querySelector(".ctrl-btn");
 const info = document.querySelector("#info");
 
@@ -51,9 +49,62 @@ export const game = () => {
   start.addEventListener("click", () => {
     randomBtn.remove();
     start.remove();
+    gameFlow();
   });
 
-  switchTurns();
+  const playerOne = {
+    name: player1.name,
+    gameOver: player1.board.gameOver(),
+    board: player1.board.board,
+    ships: player1.board.ships,
+}
+
+const playerTwo = {
+    name: player2.name,
+    gameOver: player2.board.gameOver(),
+    board: player2.board.board,
+    ships: player2.board.ships,
+}
+
+const buttonsPlayer2 = handleBtns("p2-board");
+
+
+
+// The game really starts here
+  const gameFlow = () => {
+    const switchTurns = (current) => {
+        if (current === playerOne.name) {
+            current = playerTwo.name;
+        } else {
+            current = playerOne.name;
+        }
+        console.log(current)
+        return current;
+    };
+    
+    let current = playerOne.name;
+
+    buttonsPlayer2.forEach((e) => {
+        e.addEventListener("click", () => {
+            let play = playerBtns(e, player2Board);
+            console.log(play)
+            typeWriter(info, `${current}'s turn`);
+
+            while (play === "Play again") {
+                play = playerBtns(e, player2Board);
+            }
+
+            switchTurns(current);
+            typeWriter(info, `${current}'s turn`);
+            let compPlay = computerPlay(player1Board);
+
+            while (compPlay === "Play Again") {
+                compPlay = computerPlay(player1Board);
+            }
+        });
+    });
+        console.log(current);
+    }     
 };
 
 const playButtons = () => {
@@ -62,10 +113,48 @@ const playButtons = () => {
   ctrlDiv.insertBefore(randomizeBtn, start);
 };
 
-const switchTurns = (Player = "Ressens") => {
-  typeWriter(info, `${Player}'s turn`);
-};
 
-export function handleBtnClick() {
-  console.log("Clicked");
+const handleBtns = (board) => {
+    let buttons = [];
+
+    for (let i = 0; i < 10; i++) {
+        for (let j = 0; j < 10; j++) {
+            const button = document.querySelector(`#${board}-h${i}${j}`);
+            buttons.push(button);
+        }
+    }
+    return buttons;
+}
+
+const computerPlay = (player1Board) => {
+    const x = Math.floor(Math.random() * 10);
+    const y = Math.floor(Math.random() * 10);
+
+    const play = document.querySelector(`#p1-board-${x}${y}`);
+    console.log(play)
+
+    if (play.classList.contains("ship")) {
+        play.classList.add("revealed");
+        play.classList.add("hit");
+        player1Board.receiveAttack(x, y);
+        return "Play Again";
+    } else {
+        player1Board.receiveAttack(x, y);
+        play.classList.add("revealed");
+        play.classList.add("miss");
+    }
+}
+
+const playerBtns = (e, board) => {
+    if (e.classList.contains("ship")) {
+        e.classList.add("revealed");
+        e.classList.add("hit");
+        board.receiveAttack(e.dataset.x, e.dataset.y);
+        return "Play Again";
+    } else {
+        board.receiveAttack(e.dataset.x, e.dataset.y);
+        e.classList.add("revealed");
+        e.classList.add("miss");
+        return "Someone else's turn"
+    }
 }
